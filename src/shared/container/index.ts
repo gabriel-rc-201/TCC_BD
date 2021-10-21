4f9ebb33290bac99426193c0ba67e3dd3b0ff67a
gabrieldk <ganriel201@gmail.com>
2021-10-21 15:00:09 -0300

    sigleton universidade repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,15 @@
 import { container } from "tsyringe";
 import { IAreaEstudoRepository } from "../../modules/areasEstudo/repositories/IAreasEstudoRepository";
 import { AreasEstudoRepository } from "../../modules/areasEstudo/repositories/implementations/AreasEstudoRepository";
+import { UniversidadeRepository } from "../../modules/universidades/repositories/implementations/UniversidadesRepository";
+import { IUniversidadeRepository } from "../../modules/universidades/repositories/IUniversidadesRepository";
 
 container.registerSingleton<IAreaEstudoRepository>(
   "AreasEstudoRepository",
   AreasEstudoRepository
+);
+
+container.registerSingleton<IUniversidadeRepository>(
+  "UniversidadeRepository",
+  UniversidadeRepository
 );
