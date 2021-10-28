dfe23b60694363b9608a906b136f03c6cb3d1f99
gabrieldk <ganriel201@gmail.com>
2021-10-28 11:17:19 -0300

    orientadores singleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,10 +2,12 @@ import { container } from "tsyringe";
 
 import { IAutoresRepository } from "../../modules/accounts/repositories/IAutoresRepository";
 import { AutoresRepository } from "../../modules/accounts/repositories/implementations/AutoresRepository";
+import { IOrientadoresRepository } from "../../modules/accounts/repositories/IOrientadoresRepository";
+import { OrientadoresRepository } from "../../modules/accounts/repositories/implementations/OrientadoresRepository";
 import { IAreaEstudoRepository } from "../../modules/areasEstudo/repositories/IAreasEstudoRepository";
 import { AreasEstudoRepository } from "../../modules/areasEstudo/repositories/implementations/AreasEstudoRepository";
-import { UniversidadeRepository } from "../../modules/universidades/repositories/implementations/UniversidadesRepository";
 import { IUniversidadeRepository } from "../../modules/universidades/repositories/IUniversidadesRepository";
+import { UniversidadeRepository } from "../../modules/universidades/repositories/implementations/UniversidadesRepository";
 
 container.registerSingleton<IAreaEstudoRepository>(
   "AreasEstudoRepository",
@@ -20,4 +22,9 @@ container.registerSingleton<IUniversidadeRepository>(
 container.registerSingleton<IAutoresRepository>(
   "AutoresRepository",
   AutoresRepository
+);
+
+container.registerSingleton<IOrientadoresRepository>(
+  "OrientadoresRepository",
+  OrientadoresRepository
 );
