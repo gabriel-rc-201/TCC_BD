5b324720b4efe473382e757bc1b754d5357638ca
gabrieldk <ganriel201@gmail.com>
2021-11-10 13:49:41 -0300

    interface do repositório dos cursos dos campi

---
+++ src/modules/universidades/repositories/ICursoRepository.ts
@@ -0,0 +1,20 @@
+import { Curso } from "../entities/curso";
+
+interface ICursoDTO {
+  id: Number;
+  nome: string;
+  campusid: Number;
+  universidadeid: Number;
+  turno: "diurno" | "noturno" | "integral";
+  modalidade: "licenciatura" | "bacharelado" | "bacharelado e licenciatura";
+}
+
+interface ICursoRepository {
+  create(curso: ICursoDTO): Promise<void>;
+  listCursoCampus(campusid: Number): Promise<Curso[]>;
+  listCursoUniversidade(universidadeid: Number): Promise<Curso[]>;
+  findByNome(): Promise<Curso>;
+  findMaxId(): Promise<number>;
+}
+
+export { ICursoRepository };
