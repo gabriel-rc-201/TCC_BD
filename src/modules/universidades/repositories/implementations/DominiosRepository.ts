669a057acdd661ee7852b77a514d957f59d5c5bf
gabrieldk <ganriel201@gmail.com>
2021-11-10 15:12:19 -0300

    implementação do repositório de dominios

---
+++ src/modules/universidades/repositories/implementations/DominiosRepository.ts
@@ -0,0 +1,28 @@
+import { getRepository, Repository } from "typeorm";
+import { Dominio } from "../../entities/dominio";
+import { IDominioRepository, IDominiosDTO } from "../IDominiosRepository";
+
+class DominiosRepository implements IDominioRepository {
+  private repository: Repository<Dominio>;
+
+  constructor() {
+    this.repository = getRepository(Dominio);
+  }
+
+  async findeByNome(nome: string): Promise<Dominio> {
+    const dominio = await this.repository.findOne({ dominio: nome });
+    return dominio;
+  }
+
+  async list(): Promise<Dominio[]> {
+    const dominios = await this.repository.find();
+    return dominios;
+  }
+
+  async create({ dominio, universidadeid }: IDominiosDTO): Promise<void> {
+    const newDomain = this.repository.create({ dominio, universidadeid });
+    await this.repository.save(newDomain);
+  }
+}
+
+export { DominiosRepository };
