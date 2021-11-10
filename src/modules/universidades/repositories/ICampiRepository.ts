eec0378bf0dd0c9aba2f49017a524c001f5ddff0
gabrieldk <ganriel201@gmail.com>
2021-11-10 13:49:09 -0300

    interface do repositório dos campi das universidades

---
+++ src/modules/universidades/repositories/ICampiRepository.ts
@@ -0,0 +1,16 @@
+import { Campus } from "../entities/campus";
+
+interface ICampusDTO {
+  id: Number;
+  nome: string;
+  universidadeid: Number;
+}
+
+interface ICampiRepository {
+  listCampiUniversidade(universidadeid: Number): Promise<Campus[]>;
+  findByNome(nome: string): Promise<Campus>;
+  create({ id, nome, universidadeid }: ICampusDTO): Promise<void>;
+  findMaxId(): Promise<number>;
+}
+
+export { ICampiRepository, ICampusDTO };
