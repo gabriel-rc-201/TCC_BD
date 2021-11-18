2fe3b6cd0577a6006bc8ce2f85d97a175721dbdf
gabrieldk <ganriel201@gmail.com>
2021-11-18 08:29:59 -0300

    documentação das rotas de dominio e de campus

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
 import "reflect-metadata";
 import express, { NextFunction, Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
 
 import "./database";
-
 import "./shared/container";
-
 import { router } from "./routes";
 import { AppError } from "./errors/AppErros";
+import swaggerDocs from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(router);
 
