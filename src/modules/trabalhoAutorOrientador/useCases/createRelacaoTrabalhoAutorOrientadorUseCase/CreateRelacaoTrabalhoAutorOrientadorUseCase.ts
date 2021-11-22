a894c3ce255da748076b0b242efc190a365d56f9
gabrieldk <ganriel201@gmail.com>
2021-11-22 18:17:39 -0300

    use case pra criar a relação entre Trabalho Autor e Orientador

---
+++ src/modules/trabalhoAutorOrientador/useCases/createRelacaoTrabalhoAutorOrientadorUseCase/CreateRelacaoTrabalhoAutorOrientadorUseCase.ts
@@ -0,0 +1,59 @@
+import { inject, injectable } from "tsyringe";
+import { AppError } from "../../../../errors/AppErros";
+import { IAutoresRepository } from "../../../accounts/autor/repositories/IAutoresRepository";
+import { IOrientadoresRepository } from "../../../accounts/orientador/repositories/IOrientadoresRepository";
+import { ITrabalhoAcademicoRepository } from "../../../trabalhoAcademico/repositories/ITrabalhoAcademicoRepository";
+import { ITrabalhoAutorOrientadorRepository } from "../../repositories/ITrabalhoAutorOrientadorRepository";
+
+@injectable()
+class CreateRealacaoTrabalhoAutorOrientador {
+  constructor(
+    @inject("RelacaoTrabalhoAutorOrientador")
+    private relacaoRepository: ITrabalhoAutorOrientadorRepository,
+
+    @inject("TrabalhoAcademico")
+    private trabalhoRepository: ITrabalhoAcademicoRepository,
+
+    @inject("OrientadoresRepository")
+    private orientadorRepository: IOrientadoresRepository,
+
+    @inject("AutoresRepository")
+    private autorRepository: IAutoresRepository
+  ) {}
+
+  async execute(
+    autorid: number,
+    trabalhoacademicoid: number,
+    orientadorid: number
+  ): Promise<void> {
+    const trabalhoExists = await this.trabalhoRepository.findById(
+      trabalhoacademicoid
+    );
+    if (!trabalhoExists)
+      throw new AppError(
+        "Falha ao registrar o trabalho acadêmico na relação, trabalho não encontrado"
+      );
+
+    const autorExists = await this.autorRepository.findById(autorid);
+    if (!autorExists)
+      throw new AppError(
+        "Falha ao registrar o autor acadêmico na relação, autor não encontrado"
+      );
+
+    const orientadorExists = await this.orientadorRepository.findById(
+      orientadorid
+    );
+    if (!orientadorExists)
+      throw new AppError(
+        "Falha ao registrar o orientador acadêmico na relação, autor não encontrado"
+      );
+
+    await this.relacaoRepository.create(
+      autorid,
+      trabalhoacademicoid,
+      orientadorid
+    );
+  }
+}
+
+export { CreateRealacaoTrabalhoAutorOrientador };
