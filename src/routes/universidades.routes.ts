ac287334af9a97bfdea694ae7574c7a5aa6c5a63
gabrieldk <ganriel201@gmail.com>
2021-11-23 16:19:29 -0300

    refatoração nome de arquivo

--- src/routes/universidades.routes.ts
+++ src/routes/universidades.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 
 import { CreateUniversidadeController } from "../modules/universidades/useCases/universidadesUseCases/createUniversidade/CreateUniversidadeController";
-import { ImportUniversidadeController } from "../modules/universidades/useCases/universidadesUseCases/importUniversidade/ImportAreaEstudoController";
+import { ImportUniversidadeController } from "../modules/universidades/useCases/universidadesUseCases/importUniversidade/ImportUniversidadeController";
 import { ListUniversidadeController } from "../modules/universidades/useCases/universidadesUseCases/listUniversidade/ListUniversidadeController";
 
 const universidadesRoutes = Router();
